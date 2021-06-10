4df868331ccd203f203aa70d6f36390cd1dfc54c
Owen <bwq0513@gmail.com>
2021-06-10 11:07:48 +0800

    optimization code

--- Flop Game.js
+++ Flop Game.js
@@ -67,19 +67,23 @@ const view = {
         return number
     }
   },
-  flipCard(card) {
-    if (card.classList.contains('back')) {
-      // 回傳正面
-      card.classList.remove('back')
-      card.innerHTML = this.getCardContent(Number(card.dataset.index))
-    } else {
-      // 回傳背面
-      card.classList.add('back')
-      card.innerHTML = null
-    }
+  flipCards(...cards) {
+    cards.forEach(card => {
+      if (card.classList.contains('back')) {
+        // 回傳正面
+        card.classList.remove('back')
+        card.innerHTML = this.getCardContent(Number(card.dataset.index))
+      } else {
+        // 回傳背面
+        card.classList.add('back')
+        card.innerHTML = null
+      }
+    })
   },
-  pairCard(card) {
-    card.classList.add('paired')
+  pairCards(...cards) {
+    cards.forEach(card => {
+      card.classList.add('paired')
+    })
   }
 }
 
@@ -113,7 +117,7 @@ const controller = {
 
       case GAME_STATE.FirstCardAwaits:
         //翻牌
-        view.flipCard(card)
+        view.flipCards(card)
         // 將選到的卡片加入 revealedCards
         model.revealedCards.push(card)
         this.currentState = GAME_STATE.SecondCardAwaits
@@ -121,7 +125,7 @@ const controller = {
 
       case GAME_STATE.SecondCardAwaits:
         //翻牌
-        view.flipCard(card)
+        view.flipCards(card)
         // 將選到的卡片加入 revealedCards
         model.revealedCards.push(card)
         // 判斷配對是否成功
@@ -129,7 +133,7 @@ const controller = {
         if (model.isRevealCadrsMatched()) {
           // 配對成功
           this.currentState = GAME_STATE.CardsMatched
-          model.revealedCards.forEach(card => view.pairCard(card))
+          view.pairCards(...model.revealedCards)
           // 清空revealedCards
           model.revealedCards.length = 0
           // 設定回初始狀態
@@ -137,18 +141,19 @@ const controller = {
         } else {
           // 配對失敗
           this.currentState = GAME_STATE.CardsMatchFailed
-          setTimeout(() => {
-            model.revealedCards.forEach(card => view.flipCard(card))
-            // 清空revealedCards
-            model.revealedCards.length = 0
-            // 設定回初始狀態
-            this.currentState = GAME_STATE.FirstCardAwaits
-          }, 1000)
+          setTimeout(this.resetCards, 1000)
         }
         break
     }
     console.log('this.currentState', this.currentState)
     console.log('revealedCards', model.revealedCards.map(card => card.dataset.index))
+  },
+  resetCards() {
+    view.flipCards(...model.revealedCards)
+    // 清空revealedCards
+    model.revealedCards.length = 0
+    // 設定回初始狀態
+    controller.currentState = GAME_STATE.FirstCardAwaits //這裡不能用this,this 的對象變成了 setTimeout, 所以要用controller
   }
 }
 
